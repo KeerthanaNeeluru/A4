2705597e196c65793497d459f6f27fd4bd7c26e9
keerthana <keerthananeeluru2001@gmail.com>
2023-07-13 14:17:01 +0530

    commit :1

---
+++ A4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number : ");
+            num=int.Parse(Console.ReadLine());
+            if (num > 0)
+            {
+
+                Console.WriteLine("The number is positive");
+                
+            }
+            else if(num<0){
+                Console.WriteLine("The number is negative");
+            }
+            else if (num == 0)
+            {
+                Console.WriteLine("Th number is Zero ");
+            }
+            var res = (num % 2 == 0) ? "The number is even" : "The number is odd";
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
